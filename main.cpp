b5d473ff5ae447b4084d39162e833e69e3ac87ff
Jakub Pietrzak <kubapietrzak7@gmail.com>
2023-07-06 12:16:29 +0200

    Board setup 1
    Board loads successfully; working on pieces

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <iostream>
 
 using namespace sf;
 
 int main(){
-    RenderWindow window(VideoMode(453,453),"the chess");
+    std::string thisDir = "desktop/github-projects/Kenobi-bot-chess-engine/";
+    const int windowX=600,windowY=600;
+    RenderWindow window(VideoMode(windowX,windowY),"the chess");
 
     Texture boardTexture;
-    boardTexture.loadFromFile("empty.png");
+    Texture pieceTexture;
+    boardTexture.loadFromFile(thisDir+"img/chessBoard.png",IntRect(0,0,windowX,windowY));
+    pieceTexture.loadFromFile(thisDir+"img/pieces.png");
 
     Sprite boardSprite(boardTexture);
+    Sprite pieceSprite(pieceTexture);
 
     while (window.isOpen())
     {
@@ -23,6 +29,7 @@ int main(){
 
         window.clear();
         window.draw(boardSprite);
+        window.draw(pieceSprite);
         window.display();
     }
     return 0;
